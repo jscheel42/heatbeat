e3f99c1f48d18c0ec72edd7b781b971a097132b0
Joshua Scheel <jscheel42@gmail.com>
2017-02-16 22:32:26 -0800

    add ci script

---
+++ ci_vars.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sed -i -e "s/\${PORT}/$PORT/g" \
+  -e "s/\${HOST}/$HOST/g" \
+  -e "s/\${SECRET_KEY_BASE}/$SECRET_KEY_BASE/g" \
+  -e "s/\${DB_HOST}/$DB_HOST/g" \
+  -e "s/\${DB_PORT}/$DB_PORT/g" \
+  -e "s/\${DB_NAME}/$DB_NAME/g" \
+  -e "s/\${DB_USER}/$DB_USER/g" \
+  -e "s/\${DB_PASS}/$DB_PASS/g" config/prod.exs
+  
