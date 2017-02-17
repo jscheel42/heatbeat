59a546e5e02a50d543634c7dd0a1e2f66c6ecb22
Joshua Scheel <jscheel42@gmail.com>
2017-02-16 22:45:02 -0800

    fix typo in prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :heatbeat, Heatbeat.Endpoint,
   http: [port: "${PORT}"],
-  url: [host: "${HOST}", "${PORT}"}],
+  url: [host: "${HOST}", "${PORT}"],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: "${SECRET_KEY_BASE}",
   server: true,
